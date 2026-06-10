ec3af325456128da73d2fb741342ba94eb4762ef
anusree <anusreemanojmk2004@gmail.com>
2026-06-10 22:00:19 +0530

    app.js changed

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server running on port 3000");
+});
 
 let tasks = [];
 
