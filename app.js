c487b444130b145f59d9f01832423a2f360acaa6
anusree <anusreemanojmk2004@gmail.com>
2026-06-10 13:33:14 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+let tasks = [];
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index", { tasks });
+});
+
+app.post("/add", (req, res) => {
+    const task = req.body.task;
+
+    if (task && task.trim() !== "") {
+        tasks.push(task);
+    }
+
+    res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    tasks.splice(id, 1);
+    res.redirect("/");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
