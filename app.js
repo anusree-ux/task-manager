c4d71287211c79f5d2ef1ffe23001c7ecc0ed5c6
anusree <anusreemanojmk2004@gmail.com>
2026-06-11 12:28:50 +0530

    Add MongoDB support

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Task = require("./models/Task");
 const app = express();
 const PORT = 3000;
 
-// Connect to MongoDB (Fallback to localhost if environmental variable isn't set)
-const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/taskmanager";
+const mongoURI = process.env.MONGO_URI || "mongodb://mongodb:27017/taskmanager";
+
 mongoose.connect(mongoURI)
     .then(() => console.log("📦 MongoDB connected successfully"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
